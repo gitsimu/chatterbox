dd555941617511e53d2ae44de335af3bb9e3827f
JeongYuseong <j_6367@naver.com>
2021-09-08 13:53:13 +0900

    - 웹팩 빌드파일 contenthash 제거

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.[contenthash].js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "../build")
   },
   mode: "production",
